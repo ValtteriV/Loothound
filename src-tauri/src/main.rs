3658a520d1daea8cea6b99df7964311b3437ec7c
ValtteriV <valtteri.vistiaho@gmail.com>
2023-09-16 19:48:13 +0300

    enable logging

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -8,6 +8,7 @@ fn main() {
     tauri::Builder::default()
         .plugin(oauth::init())
         .plugin(sql::init())
+        .plugin(tauri_plugin_log::Builder::default().build())
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
